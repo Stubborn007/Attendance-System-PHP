5cc994f8c97e60273d7bed5710d8be69ec1ecec0
Stubborn007 <rahulprajapati101202@gmail.com>
2026-04-12 14:54:17 +0530

    Update heading text for attendance application

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('includes/main.function.inc.php');
 <link rel="stylesheet" href="styles/index.css">
 <section class="container cover-body">
     <div class="col-lg-6">
-        <h1>Make your <strong>Attendance</strong> easy, reliable and fast</h1>
+        <h1>Make yours <strong>Attendance</strong> easy, reliable and fast</h1>
         <p class="mb-4">This is a web application made with PHP as a project and it gives you a fully functional interface to take and manage your attendance and student's data</p>
         <div>
             <a href="auth.php" class="btn btn-primary mr-2 mb-2">Get Started</a>
